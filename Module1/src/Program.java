71c21e9cb0d5af328a67887cd4a1feb4a9d02795
CrungusBungus <48503425+CrungusBungus@users.noreply.github.com>
2024-01-28 18:09:38 -0700

    Add files via upload

---
+++ Module1/src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("Bonzu");
+		System.out.println("Pippinpaddleopsicopolis III");
+		System.out.println("1234 Avatar Avenue");
+		System.out.println("Republic City");
+		System.out.println("40004");
+	}
+}
